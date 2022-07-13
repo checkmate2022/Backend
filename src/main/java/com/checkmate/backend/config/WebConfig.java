448c18ecc5e4b29a8aca8aa3b67fe3604d03e94a
jifrozen <answldjs1836@naver.com>
2022-07-13 22:22:22 +0900

    feat : chat config

---
+++ src/main/java/com/checkmate/backend/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.checkmate.backend.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.checkmate.backend.config.Filter.CORSFilter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Bean
+	public FilterRegistrationBean getFilterRegistrationBean(){
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean(new CORSFilter());
+		registrationBean.addUrlPatterns("/**");
+		return registrationBean;
+	}
+
+	// CORS 추가
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("*://*")
+			.allowedHeaders("*")
+			.allowedMethods("*");
+	}
+}
